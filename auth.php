377e8731f92c183cd9f0e9ad4c11b1e123faeec2
Damien Laurent <hiboudev@gmail.com>
2018-09-18 15:40:46 +0200

    auth.php sample

---
+++ auth.php
@@ -0,0 +1,15 @@
+<?php
+
+$PRODUCTION = false;
+
+if ($PRODUCTION) {
+    $dbAddress = "127.0.0.1";
+    $dbUserName = "yourusername";
+    $dbPassword = "yourpassword";
+} else {
+    $dbAddress = "127.0.0.1";
+    $dbUserName = "yourusername";
+    $dbPassword = "yourpassword";
+}
+
+?>
