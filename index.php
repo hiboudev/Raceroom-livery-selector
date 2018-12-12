c0733f3f85b10b115058cac0a973dd42c5f50987
Damien Laurent <hiboudev@gmail.com>
2018-12-12 16:33:21 +0100

    Shorter animations.

--- index.php
+++ index.php
@@ -87,17 +87,17 @@
             .username:hover {text-decoration:underline; color:#313da1;}
             .notification {background-color: #397fbf; font-weight: bold; color:#ddd; position: absolute; right: 0; top:0.8em; margin-top: 9px; padding: 4px; display: none}
 
-            .carName {color: #444; padding-top: 15px; padding-bottom: 8px; background-image: linear-gradient(to bottom, #eaeaea, #fafafa); border-top: 1px solid #aaa; animation: fadeIn .3s ease-in-out;}
+            .carName {color: #444; padding-top: 15px; padding-bottom: 8px; background-image: linear-gradient(to bottom, #eaeaea, #fafafa); border-top: 1px solid #aaa; animation: fadeIn .1s ease-out;}
             .carName:first-child {margin-top: 0px}
             .carName:not(:first-child) {margin-top: 38px;}
 
             .thumbnailContainer {text-align:center; margin: 0 auto; }
 
-            .thumbnail {position: relative; display: inline-block; cursor: pointer; background-image: linear-gradient(to top, #fafafa, #cecece 20%, #fafafa 87%); border-bottom: 1px solid transparent; margin-bottom: 10px; transition: border 0.3s; animation: fadeIn .3s ease-in-out;}
+            .thumbnail {position: relative; display: inline-block; cursor: pointer; background-image: linear-gradient(to top, #fafafa, #cecece 20%, #fafafa 87%); border-bottom: 1px solid transparent; margin-bottom: 10px; transition: border 0.3s; animation: fadeIn .3s ease-out;}
             .thumbnail:hover {background-image: linear-gradient(to top, #e0e0e0, #cdcdcd 20%, #fafafa 87%); border-bottom: 1px #aaa solid;}
 
             .image {z-index:0; opacity: 0}
-            .image.loaded { opacity: 1; animation: fadeIn 0.3s ease-in-out;}
+            .image.loaded { opacity: 1; animation: fadeIn 0.22s ease-out;}
             @media screen and (max-width: 460px) {
                 .image {max-width: 100%;}
             }
