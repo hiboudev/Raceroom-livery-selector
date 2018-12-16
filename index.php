e5f28ed9ed3642ef62f5acd9d13fc3b6dbdde776
Damien Laurent <hiboudev@gmail.com>
2018-12-16 16:21:02 +0100

    No text cursor over special "S".

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" type="text/css" href="styles/style-1.0.7.css">
+        <link rel="stylesheet" type="text/css" href="styles/style-1.0.8.css">
 
         <?php include "r3e_db_api.php";?>
         <script src="js/jquery-3.3.1.min.js"></script>
